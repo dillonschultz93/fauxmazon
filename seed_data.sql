ae0ca7075ff287486fea26a91a32e03002533e43
Dillon Schultz <dillonschultz93@gmail.com>
2018-01-28 12:33:17 -0700

    Created initial seed data

---
+++ seed_data.sql
@@ -0,0 +1,33 @@
+USE fauxmazonDB;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The BFG", 'Books', 9.99, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Roller Blades", 'Sports', 49.99, 2);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Coconut Oil", 'Health', 5.52, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("6 foot USB-C cable", 'Electronics', 11.62, 7);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wireless Security Camera", 'Camera and Photo', 25.40, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Diaper Bag", 'Baby', 28.64, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Paint Brush Set", 'Arts and Crafts', 11.72, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("GOJO", 'Automotive', 15.08, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone X", 'Cell Phones', 1000000.00, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Naturalist", 'Books', 6.99, 5);
+
+SELECT * FROM products
